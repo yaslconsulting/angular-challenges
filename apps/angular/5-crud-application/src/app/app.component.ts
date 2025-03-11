92afff09523d8087187d6c2e4c8d4e1362a12b9b
Yannick Safraoui <dev@yasl.fr>
2025-03-11 18:14:54 +0100

    fix(refacto): added OnPush and signal

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { Todo } from './model/todo.model';
 import { TodoService } from './services/todo.service';
 
@@ -7,27 +13,31 @@ import { TodoService } from './services/todo.service';
   imports: [CommonModule],
   selector: 'app-root',
   template: `
-    <div *ngFor="let todo of todos">
+    <div *ngFor="let todo of todos()">
       {{ todo.title }}
       <button (click)="update(todo)">Update</button>
     </div>
   `,
   styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  todos!: Todo[];
+  todos = signal<Todo[]>([]);
 
   todoService = inject(TodoService);
 
   ngOnInit(): void {
     this.todoService.getTodos().subscribe((todos) => {
-      this.todos = todos;
+      this.todos.set(todos);
     });
   }
 
   update(todo: Todo) {
     this.todoService.updateTodo(todo).subscribe((todoUpdated: Todo) => {
-      this.todos[todoUpdated.id - 1] = todoUpdated;
+      const updatedTodos = this.todos().map((todo) =>
+        todo.id === todoUpdated.id ? todoUpdated : todo,
+      );
+      this.todos.set(updatedTodos);
     });
   }
 }
